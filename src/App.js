260b363bf08b2d957f7de5cc6c5459546389c259
John Carlo Fababeir <nhojbernal38@gmail.com>
2020-08-17 17:08:15 +0800

    Assigment 1

--- src/App.js
+++ src/App.js
@@ -14,22 +14,18 @@ class App extends Component {
 			};
 		}
 
-
-
-
-
 	render() {
-    return (
-        <div>
-			<Navbar dark color="primary">
-				<div className="container">
-					<NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-				</div>
-			</Navbar>
-			<Menu dishes={this.state.dishes} />
-        </div>
-    );
-    }
-}
+		return (
+			<div>
+				<Navbar dark color="primary">
+					<div className="container">
+						<NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+					</div>
+				</Navbar>
+				<Menu dishes={this.state.dishes} />
+			</div>
+			);
+		}
+	}
 
 export default App;
