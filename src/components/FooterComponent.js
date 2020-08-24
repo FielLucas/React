e4cb9d53716aa9e6bc42a34943e8398342d039e6
John Carlo Fababeir <nhojbernal38@gmail.com>
2020-08-24 13:45:41 +0800

    React Router

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer(props) {
     return(
@@ -7,23 +8,23 @@ function Footer(props) {
             <div className="row justify-content-center">             
                 <div className="col-4 offset-1 col-sm-2">
                     <h5>Links</h5>
-                    <ul className="list-unstyled">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Menu</a></li>
-                        <li><a href="contactus.html">Contact</a></li>
+                    <ul className="list-unstyled"> 
+                        <li><Link to='/home'>Home</Link></li>
+                        <li><Link to='/aboutus'>About Us</Link></li>
+                        <li><Link to='/menu'>Menu</Link></li>
+                        <li><Link to='/contactus'>Contact Us</Link></li>
                     </ul>
                 </div>
                 <div className="col-7 col-sm-5">
                     <h5>Our Address</h5>
                     <address>
-		              121, Clear Water Bay Road<br />
-		              Clear Water Bay, Kowloon<br />
-		              HONG KONG<br />
-		              <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
-		              <i className="fa fa-fax fa-lg"></i>: +852 8765 4321<br />
-		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
-                         confusion@food.net</a>
+						121, Clear Water Bay Road<br />
+						Clear Water Bay, Kowloon<br />
+						HONG KONG<br />
+						<i className="fa fa-phone fa-lg"></i>: +693 5114 44212<br />
+						<i className="fa fa-fax fa-lg"></i>: +693 5114 4421<br />
+						<i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
+							confusion@food.net</a>
                     </address>
                 </div>
                 <div className="col-12 col-sm-4 align-self-center">
@@ -33,13 +34,13 @@ function Footer(props) {
                         <a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/"><i className="fa fa-linkedin"></i></a>
                         <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
                         <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube"></i></a>
-                        <a className="btn btn-social-icon" href="mailto:"><i className="fa fa-envelope-o"></i></a>
+                        <a className="btn btn-social-icon" href="mailto:jcfababeir30@gmail.com"><i className="fa fa-envelope-o"></i></a>
                     </div>
                 </div>
             </div>
             <div className="row justify-content-center">             
                 <div className="col-auto">
-                    <p>© Copyright 2018 Ristorante Con Fusion</p>
+                    <p>© Copyright 2020 Ristorante Con Fusion</p>
                 </div>
             </div>
         </div>
