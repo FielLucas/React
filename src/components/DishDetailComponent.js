a2e4fd3347f09066a96c527cd817b11480b93ab9
John Carlo Fababeir <nhojbernal38@gmail.com>
2020-08-25 13:45:53 +0800

    Single Page Applications Part 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap'
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 	function RenderDish({dish}) {
 		return (
-			<div className = 'col-12 col-md-5 m-1'>
+			<div>
 				<Card>
 					<CardImg width="100%" src={dish.image} alt={dish.name}/>
 						<CardBody>
@@ -16,16 +18,18 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap'
     }
     function RenderComments({comments}) {
 		return (
-			<div className = 'col-12 col-md-5 m-1'>
+			<div>
 				<h4>Comments</h4>
+				<ul className="list-unstyled">
 					{comments.map((comment) => {
 						return (
-							<div key = {comment.id}>
-								<p>{comment.comment}</p>
-								<p>{`-- ${comment.author} , ${new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}`}</p>
-							</div>
+							<li key={comment.id}>
+							<p>{comment.comment}</p>
+							<p> -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+							</li>
 						)
-				})}			
+					})}
+				</ul>	
 			</div>
 		)
     }
@@ -33,18 +37,33 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap'
     const  DishDetail = (props) => {
 	if(props.dish != null){
 		return (
-			<div className='container'>
-				<div className = 'row'>
-					<RenderDish dish={props.dish}/>
-					<RenderComments comments={props.dish.comments}/>
-				</div>
-			</div>
-		)}
-	else {
+                <div className="container">
+					<div className="row">
+						<Breadcrumb>
+
+							<BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+							<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+						</Breadcrumb>
+						<div className="col-12">
+							<h3>{props.dish.name}</h3>
+							<hr />
+						</div>                
+					</div>
+					<div className="row">
+						<div className="col-12 col-md-5 m-1">
+							<RenderDish dish={props.dish} />
+						</div>
+						<div className="col-12 col-md-5 m-1">
+							<RenderComments comments={props.comments} />
+						</div>
+					</div>
+                </div>
+            )}
+	else 
 		return (
 			<div></div>
 			)
-		}
+		
 	}
 
 export default DishDetail  
